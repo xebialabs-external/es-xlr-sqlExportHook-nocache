eefb033a2edb930f72fdb48b823840db140a9e17
Mandeep Mehra <mandeep.mehra.2012@gmail.com>
2017-03-08 09:32:45 -0600

    Made tables name configurable

--- src/main/resources/sql/InProgressReleases.py
+++ src/main/resources/sql/InProgressReleases.py
@@ -1,14 +1,22 @@
 from sql.ExportClient import ExportClient
 
+
+releaseDetailsTable =  request.query['r'] if 'r' in request.query else 'release_details'
+taskDetailsTable    =  request.query['t'] if 't' in request.query else 'task_details'
+userStoriesTable    =  request.query['u'] if 'u' in request.query else 'user_stories'
+
 releases = releaseApi.releases
 
-exportClient = ExportClient('release_details','tasks_details','user_stories')
+exportClient = ExportClient(releaseDetailsTable, taskDetailsTable, userStoriesTable)
 
+resultJson=[]
 sqlCmds = []
 for release in releases:
     if release.status!='TEMPLATE' :
-       sqlCmds.append(exportClient.get_release_details_sql(release,'JiraProject','ApplicationVersion'))
-       sqlCmds.extend(exportClient.get_tasks_details_sql(release))
-       sqlCmds.extend(exportClient.get_user_stories_sql(release,'JiraUserStories'))
+       resultJson.append({"release":exportClient.get_release_details_sql(release,'JiraProject','ApplicationVersion')})
+       for task in exportClient.get_tasks_details_sql(release):
+           resultJson.append({"task":task})
+       for userstory in exportClient.get_user_stories_sql(release,'JiraUserStories') :
+           resultJson.append({"userstory":userstory})
 
-response.entity= sqlCmds
+response.entity=resultJson
